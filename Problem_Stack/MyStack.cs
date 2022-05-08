776f4422812019e25c2944d51e5f6fc317de1a56
VladutIT <89774521+VladutIT@users.noreply.github.com>
2022-05-08 12:41:02 +0300

    Add files via upload

---
+++ Problem_Stack/MyStack.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problema1
+{
+    class MyStack
+    {/// <summary>
+    ///  count=Count
+    ///  capacity=Capacity
+    /// </summary>
+    /// 
+        private int[] data;
+        private int capacity = 16;
+        private int count = 0;
+        public MyStack()
+        {
+            data = new int[capacity];
+        }
+        public void Push(int value)
+        {
+           /// adaugam valoarea value in stiva
+           if(Count==Capacity)
+            {
+                capacity = capacity * 2;
+                int[] v = new int[capacity];
+                for(int i=0;i<Count;i++)
+                {
+                    v[i] = data[i];
+                }
+                data = v;
+                /// dublam capacitatea stivei
+            }
+            data[Count] = value;
+            count++;
+        }
+        public int Peek()
+        {
+            if(Count>0)
+            {
+                return data[Count - 1];
+            }
+            else
+            {
+                throw new Exception("Am incercat sa examinez elementul din varful unei stive goale");
+            }
+        }
+        public int Pop()
+        {
+            if(Count>0)
+            {
+                count--;
+                //se elimina din stiva elementul data[count];
+                return data[count];
+            }
+            else
+            {
+                throw new Exception("Am incercat sa elimin un element din stiva goala");
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+        public int Capacity
+        {
+            get
+            {
+                return capacity;
+            }
+        }
+
+    }
+}
---
+++ Problem_Stack/MyStackGeneric.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+public class MyStackGenerric<T>
+{
+	private T[] data;
+	private int capacity = 16;
+	private int count = 0;
+	public MyStackGenerric()
+	{
+		data = new T[capacity];
+	}
+    public void Push(T value)
+    {
+        if (Count == Capacity)
+        {
+            capacity = capacity * 2;
+            T[] v = new T[capacity];
+            for (int i = 0; i < Count; i++)
+            {
+                v[i] = data[i];
+            }
+            data = v;
+        }
+        data[Count] = value;
+        count++;
+    }
+    public T Peek()
+    {
+        if (Count > 0)
+        {
+            return data[Count - 1];
+        }
+        else
+        {
+            throw new Exception("Am incercat sa examinez elementul din varful unei stive goale");
+        }
+    }
+    public T Pop()
+    {
+
+        if (Count > 0)
+        {
+            count--;
+            return data[count];
+        }
+        else
+        {
+            throw new Exception("Am incercat sa elimin un element din stiva goala");
+        }
+    }
+    public int Count
+    {
+		get
+		{
+			return count;
+		}
+	}
+	public int Capacity
+    {
+		get
+        {
+			return capacity;
+        }
+	}
+
+
+}
