6bae614762cb1a08eb36841394521d9740825fa0
VladutIT <89774521+VladutIT@users.noreply.github.com>
2022-05-15 14:09:09 +0300

    Add files via upload

---
+++ Rectangle.cs/Point.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problem_2
+{
+    public enum Color
+    {
+        Negru,
+        Rosu,
+        Verde,
+        Albastru,
+        Galben
+    }
+    class Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public Color Culoare { get; set; }
+        public Point(): this(0,0)//x=0 y=0
+            {
+
+            }
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+            Culoare = Color.Negru;
+            Console.WriteLine($"Am creat un obiect de tip Point ({X}, {Y})");
+        }
+        public Point(int x): this(x, 0)
+        {
+       
+        }
+        public override string ToString()
+        {
+            return $"[({X}, {Y}), {this.Culoare}]";
+        }
+        public double GetDistanceToOrigin()
+        {
+            return Math.Sqrt(X * X + Y * Y);
+        }
+        public double GetDistanceTo(Point var)
+        {
+            return Math.Sqrt(Math.Pow(this.X - var.X, 2) +
+                            Math.Pow(this.Y - var.Y, 2));
+        }
+        public void MoveBy(double dx, double dy)
+        {
+            X += dx;
+            Y += dy;
+        }
+        internal void WriteToConsole()
+        {
+            ConsoleColor color = Console.ForegroundColor;
+
+            switch (Culoare)
+            {
+                case Color.Albastru:
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    break;
+                case Color.Galben:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case Color.Rosu:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case Color.Negru:
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    break;
+                case Color.Verde:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+            }
+            Console.WriteLine(this);
+
+            Console.ForegroundColor = color;
+        }
+    }
+}
---
+++ Rectangle.cs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Problem_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Point p1 = new Point { X=3.0, Y = 4.0 };
+            Point p2 = new Point();
+            Point p3 = new Point(3, 4);
+
+            Console.WriteLine(p1);
+            Point p4 = new Point(5);
+
+            Point p5 = new Point(5.0, 6.0) { Culoare = Color.Verde };
+            p5.WriteToConsole();
+
+            Console.WriteLine(p5);
+
+            Console.WriteLine($"Distanta de la origine la punctul {p1} este {p1.GetDistanceToOrigin()}");
+
+            Point p6 = new Point(1, 1);
+            p6.MoveBy(2, 2);
+            Console.WriteLine(p6);
+        }
+    }
+}
